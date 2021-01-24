7a4e8ef26eb8e4e08e34c74e668f27c65dc0e3eb
Alexander Fatyuhin <virikidorhom@gmail.com>
2021-01-24 15:45:06 +0200

    перезалил скрипт обработки заказа

--- burgers/response_form.php
+++ burgers/response_form.php
@@ -12,20 +12,91 @@ $floor   = $_POST['floor'];
 
 $comment  = $_POST['comment'];
 $payment  = $_POST['payment'];
-$callback = $_POST['callback'];
 
-$clients["$email"] = [
-    'Имя'     => $name,
-    'Телефон' => $phone,
-    'email'   => $email,
-    'Адрес'   => [
-        'улица'    => $street,
-        'дом'      => $home,
-        'корнус'   => $part,
-        'этаж'     => $floor,
-        'квартира' => $appt
-    ],
-    'коментарий' => $comment,
-    'оплата' => $payment,
-    'перезвон' => $callback
-];
+if (!empty($_POST['callback'])) {
+    $callback = $_POST['callback'];
+} else {
+    $callback = "Перезвонить";
+}
+
+echo '<html> <head></head> <body>';
+
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=loftscool", 'loftphp', 'ss135sdjfhh');
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    die;
+}
+
+if ($email) {
+    $tbClients = 'burger_clients';
+    $tbOrders = 'burger_orders';
+
+    //проверяем емейл
+    $query = $pdo->prepare("SELECT * FROM $tbClients WHERE `email` = :client_email");
+    $query->execute(['client_email' => $email]);
+    $rowCount = $query->rowCount();
+
+    if(empty($rowCount)) {
+        //создаем пользователя
+        $query = $pdo->prepare("INSERT INTO $tbClients (`email`, `name`, `phone_number`, `count_order`) values (?, ?, ?, ?)");
+        $query->execute([$email, $name, $phone, '1']);
+
+        $rowCount = $query->rowCount();
+
+        if(empty($rowCount)) {
+            echo "<p>ошибка оформления заказа 1</p>";
+            $err = $query->errorInfo();
+            var_dump($err);
+            exit();
+        } else {
+            $count = 1;
+            $id = $pdo->lastInsertId();
+            echo "id = $id <br>";
+        }
+    } else {
+        $clients = $query->fetchAll(PDO::FETCH_ASSOC);
+        $client = $clients[0];
+
+        //обновляем счетчик заказов
+        $id = $client['id'];
+        $count = ++$client['count_order'];
+        $query = $pdo->prepare("UPDATE $tbClients SET `count_order` = ? WHERE id = ?");
+        $query->execute([$count, $id]);
+
+        $rowCount = $query->rowCount();
+
+        if(empty($rowCount)) {
+            echo "<p>ошибка оформления заказа 2</p>";
+            $err = $query->errorInfo();
+            var_dump($err);
+            exit();
+        }
+    }
+
+    //записываем новый заказ
+    $date = Date('Y-m-d H:i:s');
+    $adress = "улица - $street \n дом - $home \n корпус - $part \n этаж - $floor \n квартира - $appt";
+    $desc = "коментарий - $comment \n оплата - $payment \n перезвон - $callback";
+
+    $query = $pdo->prepare("INSERT INTO $tbOrders (`client_id`, `name`, `phone_number`, `date`, `adress`, `description`) 
+    VALUES (?, ?, ?, ?, ?, ?)");
+    $query->execute([$id, $name, $phone, $date, $adress, $desc]);
+    $rowCount = $query->rowCount();
+
+    if(empty($rowCount)) {
+        echo "<p>ошибка оформления заказа 3</p>";
+        $err = $query->errorInfo();
+        var_dump($err);
+        exit();
+    } else {
+        $id_order = $pdo->lastInsertId();
+        //выводим сообщение пользователю
+        echo "<p>Спасибо, ваш заказ будет доставлен по адресу: $adress <br>
+        Номер вашего заказа: $id_order <br>
+        Это ваш $count-й заказ!</p>";
+    }
+
+}
+
+echo '</body></html>';
