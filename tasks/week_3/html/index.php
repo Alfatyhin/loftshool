3ed894319edd7309b4ac1e24a5ed55588dc754da
Alexander Fatyuhin <virikidorhom@gmail.com>
2021-01-31 12:11:03 +0200

    Системный разделитель вместо слеша

--- tasks/week_3/html/index.php
+++ tasks/week_3/html/index.php
@@ -2,8 +2,8 @@
 
 use Core\Application;
 
-include '../src/config.php';
-include '../vendor/autoload.php';
+include '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'config.php';
+include '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $app = new Application();
 $app->run();
