97913a862a964f7c24f82ff3103950e808cb2341
Alexander Fatyuhin <virikidorhom@gmail.com>
2021-02-20 07:28:15 +0200

    добавил условие чтобы сайт выводился при путой базе

--- laravel/shop/resources/views/layouts/master.blade.php
+++ laravel/shop/resources/views/layouts/master.blade.php
@@ -130,12 +130,14 @@
                     <div class="random-product-container__head">Случайный товар</div>
                     <div class="random-product-container__content">
                         <div class="item-product">
+                            @if ($randomProduct)
                             <div class="item-product__title-product"><a href="{{route('single.view', ['product' => $randomProduct])}}" class="item-product__title-product__link">{{$randomProduct->name}}</a></div>
                             <div class="item-product__thumbnail"><a href="{{route('single.view', ['product' => $randomProduct])}}" class="item-product__thumbnail__link"><img src="{{$randomProduct->image}}" alt="Preview-image" class="item-product__thumbnail__link__img"></a></div>
                             <div class="item-product__description">
                                 <div class="item-product__description__products-price"><span class="products-price">{{$randomProduct->price}} руб</span></div>
                                 <div class="item-product__description__btn-block"><a href="#" class="btn btn-blue">Купить</a></div>
                             </div>
+                            @endif
                         </div>
                     </div>
                 </div>
